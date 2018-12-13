a6d3916e3b117e55f17d61efb940bc678e0172eb
SpecialX617 <xavierlex25@gmail.com>
2018-12-13 17:24:35 -0500

    added fields to model

--- src/Model.java
+++ src/Model.java
@@ -1,4 +1,15 @@
 
 public class Model {
-
+  public String name;
+  public int qty;
+  public double costPU;
+  public double total;
+  public String date;
+  public String file;
+  public boolean expenseType;
+  
+  public Model() {
+	  
+  }
+  
 }
