be1cdc7c4f3e83465d920c32ccdeeb0a1a4a7f8c
Justin Shen <jshen94@gmail.com>
2018-12-23 23:39:49 -0500

    Added TODO's and begun framework on SQL connection and querying

--- src/Controller.java
+++ src/Controller.java
@@ -1,9 +1,45 @@
+import java.sql.*;
 
 public class Controller {
-
+	
+	private static Connection con = null;
+	private static ResultSet rs = null;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		String conURL = "jdbc:sqlserver://localhost;" +
+                "databaseName=ms_test;user=username;password=password"; //TODO change to our sql server once it is set up
+		
 		System.out.println("hello");
 	}
-
+	
+	private static void runInsertQuery(String cURL, String table, String[] values)
+		throws SQLException {
+		
+		try {
+			//TODO Fix query logic to a more automated state without having to rely on many pre-written strings
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+			con = DriverManager.getConnection(cURL);
+			con.setAutoCommit(false);
+			PreparedStatement ps = con.prepareStatement("INSERT INTO table VALUES (?,?)");
+			
+			for(int i = 1; i < values.length; i++){ //iterate through values in values[] and fill them in the statement
+													// needs to be modified to be smarter to work with any data type
+				ps.setString(i, values[i-1]);
+				
+			}
+			ps.executeUpdate();
+		con.commit();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		finally {
+			con.close();
+			con.setAutoCommit(true);
+		}
+		
+		
+	}
 }
--- src/View.java
+++ src/View.java
@@ -1,4 +1,4 @@
 
 public class View {
-
+//TODO Create a GUI so that the controller may start pulling data to fill in the queries
 }
